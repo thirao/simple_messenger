c09a15d1149b060d58bfbcd4e18892a544b4fb3c
thirao <hirao.tatsuya@gmail.com>
2018-04-10 17:43:37 +0900

    [add] アカウント機能 uiモック
    backend アカウント機能
    frontend 仮ui

---
+++ database.py
@@ -0,0 +1,108 @@
+import sqlite3
+import uuid
+class AccountDao:
+    dbname = 'account.db'
+    def __init__(self):
+        conn = sqlite3.connect(self.dbname)
+        cur = conn.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS account(id, name, password);")
+        conn.commit()
+        conn.close()
+
+
+# アカウント作成
+    def create_account(self, user, password):
+        conn = sqlite3.connect(self.dbname)
+        cur = conn.cursor()
+
+        uid = uuid.uuid4()
+        print(uid)
+        if self.is_account(user, password):
+            return "Error"
+        else:
+            pass
+
+        cur.execute("INSERT INTO account VALUES (?,?,?);", (str(uid), str(user), str(password),))
+        conn.commit()
+        conn.close()
+
+# アカウント認証
+    def is_account(self, user, password):
+        conn = sqlite3.connect(self.dbname)
+        cur = conn.cursor()
+
+        uid = uuid.uuid4()
+
+        cur.execute("SELECT name FROM account WHERE name=? AND password=?;", (user, password,))
+        result = cur.fetchall()
+        conn.close()
+        if len(result) != 0:
+            return True
+        else:
+            return False
+
+# 全件取得
+    def get_all_account(self):
+        conn = sqlite3.connect(self.dbname)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM account;")
+        result = cur.fetchall()
+        conn.close()
+        return result
+
+class MessageDao:
+    dbname = 'messages.db'
+    def __init__(self):
+        conn = sqlite3.connect(self.dbname)
+        cur = conn.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS messages(m_from, m_to, message, date);")
+        conn.commit()
+        conn.close()
+
+    def get_message_by_name(self,username):
+        conn = sqlite3.connect(self.dbname)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM messages WHERE m_to = ?;", (username,))
+        result = cur.fetchall()
+        conn.close()
+        return result
+
+    def set_message(self,message):
+        if not message.get('from', False) and message.get('to', False) and message.get('date', False) and message.get('message', False):
+            return "Error"
+
+        conn = sqlite3.connect(self.dbname)
+        cur = conn.cursor()
+
+        cur.execute(
+            "INSERT INTO messages VALUES (?,?,?,?);",
+            (message['from'], message['to'], message['message'], str(message['date']))
+        )
+        conn.commit()
+        conn.close()
+
+    def get_message(self):
+        conn = sqlite3.connect(self.dbname)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM messages;")
+        result = cur.fetchall()
+        conn.close()
+        return result
+
+
+a = AccountDao()
+
+# print(a.get_all_account())
+
+m = {
+    "from": "thirao",
+    "to": "thirao2",
+    "message": "てすと",
+    "date": "1523348346.672"
+}
+
+b = MessageDao()
+
+# print(b.set_message(m))
+
+print(b.get_message())
--- main.py
+++ main.py
@@ -9,10 +9,11 @@ import tornado.log
 from tornado.options import options, parse_command_line
 import os
 import json
-
+import logging
+import tornado.escape
+import sqlite3
 
 # debug向けの設定
-page_len = 10
 
 dummy_article = {
     "uid": 114514,
@@ -29,10 +30,15 @@ class Application(tornado.web.Application):
         # ルーティングに対応するハンドラの登録
         handlers = [
             (r"/", IndexHandler),
-            (r"/ws/", ChatHandler)
+            (r"/ws/", ChatHandler),
+            (r"/login", LoginHandler),
+            (r"/logout", LogoutHandler),
+            (r"/msg",MessageHandler)
+
         ]
         # tornadoの設定関連
         settings = dict(
+            login_url="login",
             cookie_secret='k5oy9qv76V2GWFKW3A56El3v6O33hR0M',
             static_path=os.path.join(os.path.dirname(__file__), "static"),
             template_path=os.path.join(os.path.dirname(__file__), "templates"),
@@ -43,50 +49,220 @@ class Application(tornado.web.Application):
         tornado.web.Application.__init__(self, handlers, **settings)
 
 
+class BaseHandler(tornado.web.RequestHandler):
+
+    cookie_username = "username"
+    def get_current_user(self):
+        username = self.get_secure_cookie(self.cookie_username)
+        logging.info('BaseHandler - username: %s' % username)
+        if not username: return None
+        return tornado.escape.utf8(username)
+
+    def set_current_user(self, username):
+        self.set_secure_cookie(self.cookie_username, tornado.escape.utf8(username))
+
+    def clear_current_user(self):
+        self.clear_cookie(self.cookie_username)
+
+
 # トップページと記事一覧ページのハンドラ
-class IndexHandler(tornado.web.RequestHandler):
+class IndexHandler(BaseHandler):
     @tornado.web.asynchronous
+    @tornado.web.authenticated
     def get(self):
+        uname = self.get_current_user()
         self.render(
-                "index.html"
+                "index.html",
+                uname=uname
             )
 
+class MessageHandler(BaseHandler):
+    @tornado.web.asynchronous
+    @tornado.web.authenticated
+    def get(self):
+        pass
 
-# websocket動作デモ用
+
+
+# ログインページ
+class LoginHandler(BaseHandler):
+
+    def get(self):
+        self.render("login.html")
+
+    def post(self):
+        logging.debug("xsrf_cookie:" + self.get_argument("_xsrf", None))
+        self.check_xsrf_cookie()
+
+        username = self.get_argument("username")
+        password = self.get_argument("password")
+
+        a = AccountDao()
+
+        logging.info('LoginHandler:post %s %s' % (username, password))
+
+        if a.is_account(username, password):
+            self.set_current_user(username)
+            self.redirect("/")
+        else:
+            self.write_error(403)
+
+
+# ログアウトページ
+class LogoutHandler(BaseHandler):
+    def get(self):
+        self.clear_current_user()
+        self.redirect('/')
+
+
+# メッセージやりとり用
 class ChatHandler(tornado.websocket.WebSocketHandler):
-    # ルーム一覧
-    __roomList = {}
     # ルームへ接続中のクライアント一覧
     node = set()
+    nodes = dict()
+
+    messages = []
 
     def initialize(self):
         pass
 
     def open(self):
         self.node.add(self)
+        if not self.nodes.get(self.get_secure_cookie('username'), False):
+            self.nodes.update({self.get_secure_cookie('username'): self})
+
+        logging.info(self.nodes)
+
+        # DBから会話データを取得
+        self.write_message({'message':"Hello User"})
         pass
 
     # メッセージ到着時の処理
     def on_message(self, message):
         msg = json.loads(message)
-        for w in self.node:
-            w.write_message(msg)
+        msg['from'] = self.get_secure_cookie('username').decode('utf-8')
+
+        # debug時以外dbへ格納
+        if not options.debug:
+            logging.info(options.debug)
+
+        logging.info(msg['date'])
+        for u, h in self.nodes.items():
+            logging.info(u.decode('utf-8') + ": " + msg['to'])
+            if msg['to'] == u.decode('utf-8'):
+                h.write_message(msg)
 
     # 終了時の処理
     def on_close(self):
+        d = self.nodes.pop(self.get_secure_cookie('username'))
         self.node.remove(self)
+
+
+class AccountDao:
+    dbname = 'account.db'
+
+    def __init__(self):
+        conn = sqlite3.connect(self.dbname)
+        cur = conn.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS account(id, name, password);")
+        conn.commit()
+        conn.close()
+
+    # アカウント作成
+    def create_account(self, user, password):
+        import uuid
+        conn = sqlite3.connect(self.dbname)
+        cur = conn.cursor()
+
+        uid = uuid.uuid4()
+        print(uid)
+        if self.is_account(user, password):
+            return "Error"
+        else:
+            pass
+
+        cur.execute("INSERT INTO account VALUES (?,?,?);", (str(uid), str(user), str(password),))
+        conn.commit()
+        conn.close()
+
+    # アカウント認証
+    def is_account(self, user, password):
+        import uuid
+        conn = sqlite3.connect(self.dbname)
+        cur = conn.cursor()
+
+        uid = uuid.uuid4()
+
+        cur.execute("SELECT name FROM account WHERE name=? AND password=?;", (user, password,))
+        result = cur.fetchall()
+        conn.close()
+        if len(result) != 0:
+            return True
+        else:
+            return False
+
+# 全件取得
+    def get_all_account(self):
+        conn = sqlite3.connect(self.dbname)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM account;")
+        result = cur.fetchall()
+        conn.close()
+        return result
+
+
+class MessageDao:
+    dbname = 'messages.db'
+    def __init__(self):
+        conn = sqlite3.connect(self.dbname)
+        cur = conn.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS messages(m_from, m_to, message, date);")
+        conn.commit()
+        conn.close()
+
+    def get_message_by_name(self,username):
+        conn = sqlite3.connect(self.dbname)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM messages WHERE m_to = ?;", (username,))
+        result = cur.fetchall()
+        conn.close()
+        return result
+
+    def set_message(self, message):
+        if not message.get('from', False) and message.get('to', False) and message.get('date', False) and message.get('message', False):
+            return "Error"
+
+        conn = sqlite3.connect(self.dbname)
+        cur = conn.cursor()
+
+        cur.execute(
+            "INSERT INTO messages VALUES (?,?,?,?);",
+            (message['from'], message['to'], message['message'], str(message['date']))
+        )
+        conn.commit()
+        conn.close()
+
+    def get_message(self):
+        conn = sqlite3.connect(self.dbname)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM messages;")
+        result = cur.fetchall()
+        conn.close()
+        return result
 
 
 def main():
     # tornadoのコマンドラインオプションの設定
     options.define('port', type=int, default='80')
-    options.define('debug', type=bool, default=False)
-
+    options.define('debug', type=bool, default=True)
     tornado.options.parse_command_line()
     app = Application()
-    server = tornado.httpserver.HTTPServer(app)
-    server.listen(options.port)
-    tornado.ioloop.IOLoop.current().start()
+    try:
+        server = tornado.httpserver.HTTPServer(app)
+        server.listen(options.port)
+        tornado.ioloop.IOLoop.current().start()
+    finally:
+        tornado.ioloop.IOLoop.current().stop()
 
 
 if __name__ == "__main__":
